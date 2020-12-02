e3109fd534cbcf5b74a257c4ad8c63802fc61b17
spwh <12411310+SpokeyWheeler@users.noreply.github.com>
2020-12-02 11:23:43 +0000

    Interim commit

--- cmd/batcher.go
+++ cmd/batcher.go
@@ -1,62 +1,124 @@
-package batcher
+package main
 
 import (
+	"database/sql"
+	"flag"
 	"fmt"
-	"github.com/alecthomas/kong"
+	_ "github.com/lib/pq"
+	"log"
+	"os"
 )
 
-var VERSION = "0.0.1"
-
-type Globals struct {
-	Debug     bool        `short:"D" help:"Enable debug mode"`
-	Set       string      `short:"S" help:"Set column=value"`
-	Where     string      `short:"W" help:"WHERE clause"`
-	Version   VersionFlag `name:"version" short:"V" help:"Print version information and quit"`
+type jobConfig struct {
+	command  string
+	table    string
+	set      string
+	where    string
+	batch    int
+	execute  bool
+	user     string
+	password string
+	database string
+	host     string
+	portnum  string
 }
 
-type VersionFlag string
+var (
+	VERSION = "0.1.0"
+)
 
-func (v VersionFlag) Decode(ctx *kong.DecodeContext) error { return nil }
-func (v VersionFlag) IsBool() bool                         { return true }
-func (v VersionFlag) BeforeApply(app *kong.Kong, vars kong.Vars) error {
-	fmt.Println(VERSION)
-	app.Exit(0)
-	return nil
+func parseArgs() *jobConfig {
+
+	fs := flag.NewFlagSet("update or delete", flag.ExitOnError)
+
+	tablePtr := fs.String("table", "", "table name")
+	setPtr := fs.String("set", "", "e.g. 'column_name=value, column_name=value ...' (ignored if provided with delete subcommand)")
+	wherePtr := fs.String("where", "", "e.g. 'column=value AND column IS NOT NULL ...'")
+	batchPtr := fs.Int("batch", 100, "batch size")
+	executePtr := fs.Bool("execute", false, "execute the operation ('dry-run' only by default)")
+	userPtr := fs.String("user", "", "user name")
+	passwordPtr := fs.String("password", "", "password")
+	databasePtr := fs.String("database", "", "database name")
+	hostPtr := fs.String("host", "localhost", "host name or IP")
+	portnumPtr := fs.String("portnum", "", "port number")
+
+	jc := jobConfig{command: ""}
+
+	if len(os.Args) > 1 {
+		jc.command = os.Args[1]
+	}
+
+	fs.Parse(os.Args[2:])
+
+	jc.table = *tablePtr
+	jc.where = *wherePtr
+	jc.batch = *batchPtr
+	jc.execute = *executePtr
+	jc.user = *userPtr
+	jc.password = *passwordPtr
+	jc.database = *databasePtr
+	jc.host = *hostPtr
+	jc.portnum = *portnumPtr
+
+	switch jc.command {
+	case "update":
+		jc.set = *setPtr
+	case "delete":
+		jc.set = ""
+	case "version":
+		fmt.Printf("%s Version: %s\n", os.Args[0], VERSION)
+		os.Exit(0)
+	default:
+		fmt.Println("'update', 'delete', 'version' or 'help' subcommand is required")
+		fmt.Println("flags:")
+		fs.PrintDefaults()
+		os.Exit(0)
+	}
+	return &jc
 }
 
-type CLI struct {
-	Globals
-
-	// Delete  DeleteCmd  `cmd help:"Block until one or more containers stop, then print their exit codes"`
-	// Update  UpdateCmd  `cmd help:"Block until one or more containers stop, then print their exit codes"`
-        Version VersionCmd `cmd help:"Show version information"`
+func getConnection(database string, user string, password string) (*sql.DB, error) {
+	// connStr := fmt.Sprintf("postgresql://%s:%s@localhost:26257/%s?sslmode=disable", user, password, database)
+	connStr := fmt.Sprintf("postgresql://%s@localhost:26257/%s?sslmode=disable", user, database)
+	fmt.Println(connStr)
+	// db, err := sql.Open("postgres", "postgresql://root@localhost:26257/default?sslmode=disable")
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal("error connecting to the database: ", err)
+	}
+	return db, err
 }
 
-type VersionCmd struct {
+func countRows(db *sql.DB, database string, table string, where string) (rowcount int) {
+	sqlstr := fmt.Sprintf("SELECT COUNT(1) FROM %s.%s WHERE %s;\n", database, table, where)
+	rows, err := db.Query(sqlstr)
+	if err != nil {
+		log.Fatal("error counting rows: ", err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		if err := rows.Scan(&rowcount); err != nil {
+			log.Fatal(err)
+		}
+	}
+	return rowcount
 }
 
-func (cmd *VersionCmd) Run(globals *Globals) error {
-	fmt.Println(VERSION)
-	return nil
+func do_main() {
+	jobConfig := parseArgs()
+	fmt.Println(jobConfig.command, jobConfig.database, jobConfig.table, jobConfig.set, jobConfig.where, jobConfig.batch, jobConfig.execute, jobConfig.user, jobConfig.password)
+	conn, err := getConnection(jobConfig.database, jobConfig.user, jobConfig.password)
+	if err != nil {
+		log.Fatal("error connecting to the database: ", err)
+	}
+	// rowsleft := countRows(conn, jobConfig.database, jobConfig.table, jobConfig.where)
+	// fmt.Println("Rows left: ", rowsleft)
+
+	while countRows(conn, jobConfig.database, jobConfig.table, jobConfig.where) > 0 {
+	}
 }
 
 func main() {
-	cli := CLI{
-		Globals: Globals{
-			Version: VersionFlag(VERSION),
-		},
-	}
-
-	ctx := kong.Parse(&cli,
-		kong.Name("batcher"),
-		kong.Description("Transaction-friendly bulk updates and deletes"),
-		kong.UsageOnError(),
-		kong.ConfigureHelp(kong.HelpOptions{
-			Compact: true,
-		}),
-		kong.Vars{
-			"version": VERSION,
-		})
-	err := ctx.Run(&cli.Globals)
-	ctx.FatalIfErrorf(err)
+	do_main()
 }
+
--- cmd/batcher_test.go
+++
@@ -1,5 +0,0 @@
-import (
-	"testing"
-	"batcher"
-)
-

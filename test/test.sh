cb52c91575a92d15c467eccb7532f1b99d7533c2
spwh <12411310+SpokeyWheeler@users.noreply.github.com>
2020-12-12 13:16:19 +0000

    Get rid of dummy username and password for tests

--- test/test.sh
+++ test/test.sh
@@ -33,6 +33,8 @@ printf "Preparing load script..."
 echo "DROP DATABASE IF EXISTS batchertestdb;
 CREATE DATABASE IF NOT EXISTS batchertestdb;
 USE batchertestdb;
+CREATE USER $DBUSER WITH PASSWORD '$DBPASSWORD';
+GRANT admin TO $DBUSER;
 CREATE TABLE IF NOT EXISTS serialtest (pk SERIAL NOT NULL PRIMARY KEY, intcol INT, strcol VARCHAR(20));" > /tmp/$$
 
 for i in {1..1000}
@@ -79,7 +81,7 @@ done
 echo "done"
 printf "Populating test database..."
 
-psql -q "postgresql://root@localhost:26257?sslcert=/Users/spokey/.cockroach-certs/client.root.crt&sslkey=/Users/spokey/.cockroach-certs/client.root.key&sslrootcert=/Users/spokey/.cockroach-certs/ca.crt&sslmode=verify-ca" < /tmp/$$
+psql -q "postgresql://root@localhost:5432" < /tmp/$$
 
 echo "done"
 printf "Starting tests"
@@ -87,67 +89,67 @@ printf "Starting tests"
 exptot=1000
 expa=100
 
-sertot=$( psql -q "postgresql://root@localhost:26257?sslcert=/Users/spokey/.cockroach-certs/client.root.crt&sslkey=/Users/spokey/.cockroach-certs/client.root.key&sslrootcert=/Users/spokey/.cockroach-certs/ca.crt&sslmode=verify-ca" -t -A -c "USE batchertestdb; SELECT COUNT(1) FROM serialtest;" )
+sertot=$( psql -q "postgresql://root@localhost:5432" -t -A -c "USE batchertestdb; SELECT COUNT(1) FROM serialtest;" )
 comp "Initial serial total" $exptot $sertot
-sera=$( psql -q "postgresql://root@localhost:26257?sslcert=/Users/spokey/.cockroach-certs/client.root.crt&sslkey=/Users/spokey/.cockroach-certs/client.root.key&sslrootcert=/Users/spokey/.cockroach-certs/ca.crt&sslmode=verify-ca" -t -A -c "USE batchertestdb; SELECT COUNT(1) FROM serialtest WHERE strcol = 'a';" )
+sera=$( psql -q "postgresql://root@localhost:5432" -t -A -c "USE batchertestdb; SELECT COUNT(1) FROM serialtest WHERE strcol = 'a';" )
 comp "Initial serial a" $expa $sera
-uidtot=$( psql -q "postgresql://root@localhost:26257?sslcert=/Users/spokey/.cockroach-certs/client.root.crt&sslkey=/Users/spokey/.cockroach-certs/client.root.key&sslrootcert=/Users/spokey/.cockroach-certs/ca.crt&sslmode=verify-ca" -t -A -c "USE batchertestdb; SELECT COUNT(1) FROM uuidtest;" )
+uidtot=$( psql -q "postgresql://root@localhost:5432" -t -A -c "USE batchertestdb; SELECT COUNT(1) FROM uuidtest;" )
 comp "Initial UUID total" $exptot $uidtot
-uida=$( psql -q "postgresql://root@localhost:26257?sslcert=/Users/spokey/.cockroach-certs/client.root.crt&sslkey=/Users/spokey/.cockroach-certs/client.root.key&sslrootcert=/Users/spokey/.cockroach-certs/ca.crt&sslmode=verify-ca" -t -A -c "USE batchertestdb; SELECT COUNT(1) FROM uuidtest WHERE strcol = 'a';" )
+uida=$( psql -q "postgresql://root@localhost:5432" -t -A -c "USE batchertestdb; SELECT COUNT(1) FROM uuidtest WHERE strcol = 'a';" )
 comp "Initial UUID a" $expa $uida
-cmptot=$( psql -q "postgresql://root@localhost:26257?sslcert=/Users/spokey/.cockroach-certs/client.root.crt&sslkey=/Users/spokey/.cockroach-certs/client.root.key&sslrootcert=/Users/spokey/.cockroach-certs/ca.crt&sslmode=verify-ca" -t -A -c "USE batchertestdb; SELECT COUNT(1) FROM compositetest;" )
+cmptot=$( psql -q "postgresql://root@localhost:5432" -t -A -c "USE batchertestdb; SELECT COUNT(1) FROM compositetest;" )
 comp "Initial composite total" $exptot $cmptot
-cmpa=$( psql -q "postgresql://root@localhost:26257?sslcert=/Users/spokey/.cockroach-certs/client.root.crt&sslkey=/Users/spokey/.cockroach-certs/client.root.key&sslrootcert=/Users/spokey/.cockroach-certs/ca.crt&sslmode=verify-ca" -t -A -c "USE batchertestdb; SELECT COUNT(1) FROM compositetest WHERE strcol = 'a';" )
+cmpa=$( psql -q "postgresql://root@localhost:5432" -t -A -c "USE batchertestdb; SELECT COUNT(1) FROM compositetest WHERE strcol = 'a';" )
 comp "Initial composite a" $expa $cmpa
 
 exptot=900
 expa=0
 
-../batcher update -concurrency 4 -database batchertestdb -dbtype postgres -host localhost -opts "sslmode=require" -password b3fuddled! -portnum 26257 -set "strcol='b'"  -table serialtest -user spokey -where "strcol='a'" -execute
+../batcher update -concurrency 4 -database batchertestdb -dbtype postgres -host localhost -opts "sslmode=require" -password $DBPASSWORD -portnum 5432 -set "strcol='b'"  -table serialtest -user $DBUSER -where "strcol='a'" -execute
 
-sera=$( psql -q "postgresql://root@localhost:26257?sslcert=/Users/spokey/.cockroach-certs/client.root.crt&sslkey=/Users/spokey/.cockroach-certs/client.root.key&sslrootcert=/Users/spokey/.cockroach-certs/ca.crt&sslmode=verify-ca" -t -A -c "USE batchertestdb; SELECT COUNT(1) FROM serialtest WHERE strcol = 'a';" )
+sera=$( psql -q "postgresql://root@localhost:5432" -t -A -c "USE batchertestdb; SELECT COUNT(1) FROM serialtest WHERE strcol = 'a';" )
 comp "Updated serial a" $expa $sera
 
-../batcher delete -concurrency 4 -database batchertestdb -dbtype postgres -host localhost -opts "sslmode=require" -password b3fuddled! -portnum 26257 -table serialtest -user spokey -where "intcol<101" -execute
+../batcher delete -concurrency 4 -database batchertestdb -dbtype postgres -host localhost -opts "sslmode=require" -password $DBPASSWORD -portnum 5432 -table serialtest -user $DBUSER -where "intcol<101" -execute
 
-sertot=$( psql -q "postgresql://root@localhost:26257?sslcert=/Users/spokey/.cockroach-certs/client.root.crt&sslkey=/Users/spokey/.cockroach-certs/client.root.key&sslrootcert=/Users/spokey/.cockroach-certs/ca.crt&sslmode=verify-ca" -t -A -c "USE batchertestdb; SELECT COUNT(1) FROM serialtest;" )
+sertot=$( psql -q "postgresql://root@localhost:5432" -t -A -c "USE batchertestdb; SELECT COUNT(1) FROM serialtest;" )
 comp "Small delete serial total" $exptot $sertot
 
-../batcher update -concurrency 4 -database batchertestdb -dbtype postgres -host localhost -opts "sslmode=require" -password b3fuddled! -portnum 26257 -set "strcol='b'"  -table uuidtest -user spokey -where "strcol='a'" -execute
+../batcher update -concurrency 4 -database batchertestdb -dbtype postgres -host localhost -opts "sslmode=require" -password $DBPASSWORD -portnum 5432 -set "strcol='b'"  -table uuidtest -user $DBUSER -where "strcol='a'" -execute
 
-uida=$( psql -q "postgresql://root@localhost:26257?sslcert=/Users/spokey/.cockroach-certs/client.root.crt&sslkey=/Users/spokey/.cockroach-certs/client.root.key&sslrootcert=/Users/spokey/.cockroach-certs/ca.crt&sslmode=verify-ca" -t -A -c "USE batchertestdb; SELECT COUNT(1) FROM uuidtest WHERE strcol = 'a';" )
+uida=$( psql -q "postgresql://root@localhost:5432" -t -A -c "USE batchertestdb; SELECT COUNT(1) FROM uuidtest WHERE strcol = 'a';" )
 comp "Updated UUID a" $expa $uida
 
-../batcher delete -concurrency 4 -database batchertestdb -dbtype postgres -host localhost -opts "sslmode=require" -password b3fuddled! -portnum 26257 -table uuidtest -user spokey -where "intcol<101" -execute
+../batcher delete -concurrency 4 -database batchertestdb -dbtype postgres -host localhost -opts "sslmode=require" -password $DBPASSWORD -portnum 5432 -table uuidtest -user $DBUSER -where "intcol<101" -execute
 
-uidtot=$( psql -q "postgresql://root@localhost:26257?sslcert=/Users/spokey/.cockroach-certs/client.root.crt&sslkey=/Users/spokey/.cockroach-certs/client.root.key&sslrootcert=/Users/spokey/.cockroach-certs/ca.crt&sslmode=verify-ca" -t -A -c "USE batchertestdb; SELECT COUNT(1) FROM uuidtest;" )
+uidtot=$( psql -q "postgresql://root@localhost:5432" -t -A -c "USE batchertestdb; SELECT COUNT(1) FROM uuidtest;" )
 comp "Small delete UUID total" $exptot $uidtot
 
-../batcher update -concurrency 4 -database batchertestdb -dbtype postgres -host localhost -opts "sslmode=require" -password b3fuddled! -portnum 26257 -set "strcol='b'"  -table compositetest -user spokey -where "strcol='a'" -execute
+../batcher update -concurrency 4 -database batchertestdb -dbtype postgres -host localhost -opts "sslmode=require" -password $DBPASSWORD -portnum 5432 -set "strcol='b'"  -table compositetest -user $DBUSER -where "strcol='a'" -execute
 
-cmpa=$( psql -q "postgresql://root@localhost:26257?sslcert=/Users/spokey/.cockroach-certs/client.root.crt&sslkey=/Users/spokey/.cockroach-certs/client.root.key&sslrootcert=/Users/spokey/.cockroach-certs/ca.crt&sslmode=verify-ca" -t -A -c "USE batchertestdb; SELECT COUNT(1) FROM compositetest WHERE strcol = 'a';" )
+cmpa=$( psql -q "postgresql://root@localhost:5432" -t -A -c "USE batchertestdb; SELECT COUNT(1) FROM compositetest WHERE strcol = 'a';" )
 comp "Updated composite a" $expa $cmpa
 
-../batcher delete -concurrency 4 -database batchertestdb -dbtype postgres -host localhost -opts "sslmode=require" -password b3fuddled! -portnum 26257 -table compositetest -user spokey -where "intcol<101" -execute
+../batcher delete -concurrency 4 -database batchertestdb -dbtype postgres -host localhost -opts "sslmode=require" -password $DBPASSWORD -portnum 5432 -table compositetest -user $DBUSER -where "intcol<101" -execute
 
-cmptot=$( psql -q "postgresql://root@localhost:26257?sslcert=/Users/spokey/.cockroach-certs/client.root.crt&sslkey=/Users/spokey/.cockroach-certs/client.root.key&sslrootcert=/Users/spokey/.cockroach-certs/ca.crt&sslmode=verify-ca" -t -A -c "USE batchertestdb; SELECT COUNT(1) FROM compositetest;" )
+cmptot=$( psql -q "postgresql://root@localhost:5432" -t -A -c "USE batchertestdb; SELECT COUNT(1) FROM compositetest;" )
 comp "Small delete composite total" $exptot $cmptot
 
 exptot=0
 
-../batcher delete -concurrency 4 -database batchertestdb -dbtype postgres -host localhost -opts "sslmode=require" -password b3fuddled! -portnum 26257 -table serialtest -user spokey -where "1=1" -execute
+../batcher delete -concurrency 4 -database batchertestdb -dbtype postgres -host localhost -opts "sslmode=require" -password $DBPASSWORD -portnum 5432 -table serialtest -user $DBUSER -where "1=1" -execute
 
-sertot=$( psql -q "postgresql://root@localhost:26257?sslcert=/Users/spokey/.cockroach-certs/client.root.crt&sslkey=/Users/spokey/.cockroach-certs/client.root.key&sslrootcert=/Users/spokey/.cockroach-certs/ca.crt&sslmode=verify-ca" -t -A -c "USE batchertestdb; SELECT COUNT(1) FROM serialtest;" )
+sertot=$( psql -q "postgresql://root@localhost:5432" -t -A -c "USE batchertestdb; SELECT COUNT(1) FROM serialtest;" )
 comp "Full delete serial total" $exptot $sertot
 
-../batcher delete -concurrency 4 -database batchertestdb -dbtype postgres -host localhost -opts "sslmode=require" -password b3fuddled! -portnum 26257 -table uuidtest -user spokey -where "1=1" -execute
+../batcher delete -concurrency 4 -database batchertestdb -dbtype postgres -host localhost -opts "sslmode=require" -password $DBPASSWORD -portnum 5432 -table uuidtest -user $DBUSER -where "1=1" -execute
 
-uidtot=$( psql -q "postgresql://root@localhost:26257?sslcert=/Users/spokey/.cockroach-certs/client.root.crt&sslkey=/Users/spokey/.cockroach-certs/client.root.key&sslrootcert=/Users/spokey/.cockroach-certs/ca.crt&sslmode=verify-ca" -t -A -c "USE batchertestdb; SELECT COUNT(1) FROM uuidtest;" )
+uidtot=$( psql -q "postgresql://root@localhost:5432" -t -A -c "USE batchertestdb; SELECT COUNT(1) FROM uuidtest;" )
 comp "Full delete UUID total" $exptot $uidtot
 
-../batcher delete -concurrency 4 -database batchertestdb -dbtype postgres -host localhost -opts "sslmode=require" -password b3fuddled! -portnum 26257 -table compositetest -user spokey -where "1=1" -execute
+../batcher delete -concurrency 4 -database batchertestdb -dbtype postgres -host localhost -opts "sslmode=require" -password $DBPASSWORD -portnum 5432 -table compositetest -user $DBUSER -where "1=1" -execute
 
-cmptot=$( psql -q "postgresql://root@localhost:26257?sslcert=/Users/spokey/.cockroach-certs/client.root.crt&sslkey=/Users/spokey/.cockroach-certs/client.root.key&sslrootcert=/Users/spokey/.cockroach-certs/ca.crt&sslmode=verify-ca" -t -A -c "USE batchertestdb; SELECT COUNT(1) FROM compositetest;" )
+cmptot=$( psql -q "postgresql://root@localhost:5432"USE batchertestdb; SELECT COUNT(1) FROM compositetest;" )
 comp "Full delete composite total" $exptot $cmptot
 
 rm /tmp/$$

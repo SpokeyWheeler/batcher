d41ad5af94f5260486080d69c2f7db67b97bcce0
spwh <12411310+SpokeyWheeler@users.noreply.github.com>
2021-01-01 12:04:44 +0000

    ci(travis): forgot a sudo or two

--- test/install_mariadb.sh
+++ test/install_mariadb.sh
@@ -4,14 +4,14 @@
 set -eo pipefail
 
 # install MariaDB
-cp test/my.cnf /etc/my.cnf
+sudo cp test/my.cnf /etc/my.cnf
 sudo apt install -y mariadb-server
+sudo cp test/my.cnf /etc/my.cnf
 cd /tmp
 mkdir -p mariadb
 chown mysql:mysql mariadb
 chmod 750 mariadb
 cd mariadb
-sudo cp test/my.cnf /etc/my.cnf
 sudo mariadbd --initialize-insecure --user=mysql --datadir=$ pwd )/data
 
 # done
--- test/install_mysql.sh
+++ test/install_mysql.sh
@@ -3,14 +3,14 @@
 # fail fast
 set -eo pipefile
 
+sudo cp test/my.cnf /etc/my.cnf
 sudo apt-get install -y mysql-server
-cp test/my.cnf /etc/my.cnf
+sudo cp test/my.cnf /etc/my.cnf
 cd /tmp
 mkdir -p mysql
 chown mysql:mysql mysql
 chmod 750 mysql
 cd mysql
-sudo cp test/my.cnf /etc/my.cnf
 sudo mysqld --initialize-insecure --user=mysql --datadir=$ pwd )/data
 
 # done

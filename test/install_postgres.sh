79b63ad1836a67f9d8f9a54794f94bf1ca96b3f6
spwh <12411310+SpokeyWheeler@users.noreply.github.com>
2021-01-01 22:14:42 +0000

    ci(postgres): try environment variables again

--- test/install_postgres.sh
+++ test/install_postgres.sh
@@ -20,14 +20,17 @@ sudo apt-get update
 
 # install
 sudo apt install postgresql-13 postgresql-client-13
+export PGUSER=btest
+export PGPASSWORD=btest
+export PGDATABASE=batchertestdb
 sudo pg_ctlcluster 13 main start
 sudo pg_ctlcluster 13 main status
-sudo createuser -U postgres -h localhost -p 5433 -w -d -i -s btest
-sudo createdb -U postgres -h localhost -p 5433 -w -O btest batchertestdb 
+# sudo createuser -U postgres -h localhost -p 5433 -w -d -i -s btest
+# sudo createdb -U postgres -h localhost -p 5433 -w -O btest batchertestdb 
 printf "Waiting for PostgreSQL to become available"
 while :
 do
-	sudo psql -w -h localhost -p 5433 -U postgres -l # > /dev/null 2>&1
+	sudo psql -w -h localhost -p 5433 -l # > /dev/null 2>&1
 	if [ $? -eq 0 ]
 	then
 		break

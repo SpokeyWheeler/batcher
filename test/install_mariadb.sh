ef9456102c8c3b2ed7d8b299e6c07b9dc85caeea
spwh <12411310+SpokeyWheeler@users.noreply.github.com>
2021-01-02 10:45:45 +0000

    ci(mariadb): use the mariadb repo

--- test/install_mariadb.sh
+++ test/install_mariadb.sh
@@ -5,9 +5,13 @@
 
 # install MariaDB
 sudo apt update
-sudo apt install mariadb-server
-journalctl -xe
-sudo mysql_install_db --auth-root-authentication-method=normal --verbose --force
+sudo apt -y install software-properties-common
+sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
+sudo add-apt-repository 'deb [arch=amd64] http://mariadb.mirror.globo.tech/repo/10.5/ubuntu focal main'
+sudo apt update
+sudo apt -y install mariadb-server
+# journalctl -xe
+# sudo mysql_install_db --auth-root-authentication-method=normal --verbose --force
 systemctl status mariadb
 
 # done

2df6159e02945a5d0f729c16e2a6e9164853d649
spwh <12411310+SpokeyWheeler@users.noreply.github.com>
2021-01-02 11:06:45 +0000

    ci(mariadb): use the exact same commands as MySQL 8

--- test/install_mariadb.sh
+++ test/install_mariadb.sh
@@ -10,8 +10,6 @@ sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.a
 sudo add-apt-repository 'deb [arch=amd64] http://mariadb.mirror.globo.tech/repo/10.5/ubuntu focal main'
 sudo apt update
 sudo apt -y install mariadb-server
-# journalctl -xe
-# sudo mysql_install_db --auth-root-authentication-method=normal --verbose --force
 systemctl status mariadb
 
 # done
--- test/install_mysql.sh
+++ test/install_mysql.sh
@@ -3,20 +3,11 @@
 # fail fast
 # set -eo pipefail
 
-# wget -c https://dev.mysql.com/get/mysql-apt-config_0.8.11-1_all.deb
-# sudo dpkg -i mysql-apt-config_0.8.11-1_all.deb
 sudo apt update
 sudo apt-get install -y mysql-server
-# sudo cp test/my.cnf /etc/my.cnf
-# cd /tmp
-# mkdir -p mysql
-# sudo chown mysql:mysql mysql
-# sudo chmod 750 mysql
 sudo systemctl start mysql
 sleep 10
 systemctl status mysql.service
-# sudo -s "cd mysql ; mysqld --initialize-insecure --user=mysql --datadir=$ pwd )/data"
-# cd -
 
 # done
 exit 0

4821ffa55d11b21972034c3e53ce6d6bba6d327c
Spokey Wheeler <12411310+SpokeyWheeler@users.noreply.github.com>
2021-01-03 20:40:52 +0000

    fix(postgres): provide diagnostics

--- test/mariadb.sh
+++ test/mariadb.sh
@@ -7,13 +7,14 @@
 test/create_pops.sh
 
 . test/libs.sh
-test/install_mariadb.sh
+# test/install_mariadb.sh
 
 netstat -an | grep 3306
+docker ps
+docker info semmaria
 
-#mariadb mysql -uroot -w --protocol=tcp -P3306 -hlocalhost "
-mariadb -uroot -w --protocol=tcp -P3306 -hlocalhost mysql -e "SHOW DATABASES;"
-SQLCMD0="mariadb mysql -uroot -w --protocol=tcp -P3306 -hlocalhost "
+mariadb -uroot -pbtestroot --protocol=tcp -P3306 -hlocalhost mysql -e "SHOW DATABASES;"
+SQLCMD0="mariadb mysql -uroot -pbtestroot --protocol=tcp -P3306 -hlocalhost "
 SQLCMD1="mariadb batchertestdb -s -ubtest -pbtest --protocol=tcp -P3306 -hlocalhost "
 SQLCMD="mariadb batchertestdb -s -ubtest -pbtest --protocol=tcp -P3306 -hlocalhost -e "
 
@@ -26,9 +27,9 @@ $SQLCMD0 < test/mysql1.sql # > /dev/null 2>&1
 echo "done"
 
 printf "Populating test database."
-$SQLCMD1 < /tmp/pop_serial.sql # > /dev/null 2>&1
+$SQLCMD1 < /tmp/pop_serial.sql > /dev/null 2>&1
 printf "."
-$SQLCMD1 < /tmp/pop_composite.sql # > /dev/null 2>&1
+$SQLCMD1 < /tmp/pop_composite.sql > /dev/null 2>&1
 echo ".done"
 
 printf "Starting tests"

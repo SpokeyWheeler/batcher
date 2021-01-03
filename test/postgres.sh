137b3cb803546895cccbffe2c8e06410bc94ac81
Spokey Wheeler <12411310+SpokeyWheeler@users.noreply.github.com>
2021-01-03 20:18:54 +0000

    fix(postgres): grant more permissions

--- test/postgres.sh
+++ test/postgres.sh
@@ -16,15 +16,15 @@ export SQLCMD='psql -w -h 0.0.0.0 -p 5432 -U btest -d batchertestdb -t -A -c '
 $SQLCMD "SELECT version();"
 
 printf "Creating test database..."
-$SQLCMD0 < test/postgres1.sql # > /dev/null 2>&1
+$SQLCMD0 < test/postgres1.sql #> /dev/null 2>&1
 echo "done"
 
 printf "Populating test database."
-$SQLCMD0 < /tmp/pop_serial.sql # > /dev/null 2>&1
+$SQLCMD0 < /tmp/pop_serial.sql > /dev/null 2>&1
 printf "."
-$SQLCMD0 < /tmp/pop_uuid.sql # > /dev/null 2>&1
+$SQLCMD0 < /tmp/pop_uuid.sql /dev/null 2>&1
 printf "."
-$SQLCMD0 < /tmp/pop_composite.sql # > /dev/null 2>&1
+$SQLCMD0 < /tmp/pop_composite.sql > /dev/null 2>&1
 echo "done"
 
 testcount=0

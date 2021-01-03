145276ccaa654019dd2c15faa9b9ca0d2ef5160f
Spokey Wheeler <spokey@Spokeys-MacBook.local>
2021-01-03 19:29:50 +0000

    fix(mysql): allow btest to access from any server

--- test/mysql1.sql
+++ test/mysql1.sql
@@ -5,7 +5,7 @@ CREATE DATABASE IF NOT EXISTS batchertestdb CHARACTER SET 'utf8' COLLATE 'utf8_g
 USE batchertestdb;
 CREATE TABLE IF NOT EXISTS serialtest (pk SERIAL NOT NULL PRIMARY KEY, intcol INT, strcol VARCHAR(20));
 CREATE TABLE IF NOT EXISTS compositetest (pk1 INT NOT NULL, pk2 VARCHAR(10) NOT NULL, intcol INT, strcol VARCHAR(20), PRIMARY KEY(pk1, pk2));
-DROP USER IF EXISTS 'btest'@'localhost';
-CREATE USER IF NOT EXISTS 'btest'@'localhost' IDENTIFIED BY 'btest';
-GRANT ALL PRIVILEGES ON *.* TO 'btest'@'localhost';
+DROP USER IF EXISTS 'btest'@'%';
+CREATE USER IF NOT EXISTS 'btest'@'%' IDENTIFIED BY 'btest';
+GRANT ALL PRIVILEGES ON *.* TO 'btest'@'%';
 FLUSH PRIVILEGES;

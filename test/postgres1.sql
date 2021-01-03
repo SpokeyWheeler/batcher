f4e40494820479fa146cb0e491e1affd5eba81a1
Spokey Wheeler <12411310+SpokeyWheeler@users.noreply.github.com>
2021-01-03 20:10:52 +0000

    fix(postgres): grant permissions

--- test/postgres1.sql
+++ test/postgres1.sql
@@ -1,6 +1,6 @@
 CREATE USER btest PASSWORD 'btest';
-GRANT admin TO btest;
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 CREATE TABLE IF NOT EXISTS serialtest (pk SERIAL NOT NULL PRIMARY KEY, intcol INT, strcol VARCHAR(20));
 CREATE TABLE IF NOT EXISTS uuidtest (pk UUID DEFAULT gen_random_uuid() NOT NULL PRIMARY KEY, intcol INT, strcol VARCHAR(20));
 CREATE TABLE IF NOT EXISTS compositetest (pk1 INT NOT NULL, pk2 VARCHAR(10) NOT NULL, intcol INT, strcol VARCHAR(20), PRIMARY KEY(pk1, pk2));
+GRANT ALL ON batchertestdb.* TO btest;

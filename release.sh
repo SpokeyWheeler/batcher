f8d77249ef34f44dfe7716cb0fadc59e5f0f0972
spwh <12411310+SpokeyWheeler@users.noreply.github.com>
2021-01-04 14:28:09 +0000

    fix(buddy): remove diagnostics

--- release.sh
+++ release.sh
@@ -1,6 +1,5 @@
 rm -f split.sh
 git describe
-
 cd /tmp
 curl -sfL https://install.goreleaser.com/github.com/goreleaser/goreleaser.sh | sh
 cd -
--- test/mysql.sh
+++ test/mysql.sh
@@ -18,12 +18,12 @@ SQLCMD1="mysql batchertestdb -s -ubtest -pbtest --protocol=tcp -P3306 -hmysql "
 SQLCMD="mysql batchertestdb -s -ubtest -pbtest --protocol=tcp -P3306 -hmysql -e "
 
 printf "Creating test database..."
-$SQLCMD0 < test/mysql1.sql # > /dev/null 2>&1
+$SQLCMD0 < test/mysql1.sql > /dev/null 2>&1
 echo "done"
 
 printf "Populating test database..."
-$SQLCMD1 < /tmp/pop_serial.sql # > /dev/null 2>&1
-$SQLCMD1 < /tmp/pop_composite.sql # > /dev/null 2>&1
+$SQLCMD1 < /tmp/pop_serial.sql > /dev/null 2>&1
+$SQLCMD1 < /tmp/pop_composite.sql > /dev/null 2>&1
 echo "done"
 
 printf "Starting tests"

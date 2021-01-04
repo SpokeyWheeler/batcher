0ee0bb7c53c78e91213ff6599f70e85ce86f31f7
spwh <12411310+SpokeyWheeler@users.noreply.github.com>
2021-01-04 10:40:12 +0000

    fix(postgres): fix PGPASS

--- test/postgres.sh
+++ test/postgres.sh
@@ -10,8 +10,9 @@ test/create_pops.sh
 
 . test/libs.sh
 
-export SQLCMD0='PGPASS=btest psql -w -h postgres -p 5432 -U btest -d batchertestdb '
-export SQLCMD='PGPASS=btest psql -w -h postgres -p 5432 -U btest -d batchertestdb -t -A -c '
+export PGPASS=btest
+export SQLCMD0='psql -w -h postgres -p 5432 -U btest -d batchertestdb '
+export SQLCMD='psql -w -h postgres -p 5432 -U btest -d batchertestdb -t -A -c '
 
 $SQLCMD "SELECT version();"
 

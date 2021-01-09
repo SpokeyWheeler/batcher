f657d4db39345c08b1c1c02411694949338cbbfa
Spokey Wheeler <12411310+SpokeyWheeler@users.noreply.github.com>
2021-01-09 19:48:34 +0000

    ci(maria): align with mysql

--- test/mariadb.sh
+++ test/mariadb.sh
@@ -1,33 +1,17 @@
 #!/bin/bash
 
 # fail fast
-# set -eo pipefail
+set -eo pipefail
 
-# ./build.sh
 test/create_pops.sh
 
 . test/libs.sh
-# test/install_mariadb.sh
 
-printf "Waiting for MariaDB to come up"
-for i in {1..10}
-do
-mariadb -hmysql -uroot -pbtestroot --protocol=tcp mysql -e "SHOW DATABASES;" > /dev/null 2>&1
-if [ $? -eq 0 ]
-then
-break
-fi
-printf "."
-sleep 1
-done
-echo "done"
-
-
-# mariadb -uroot -pbtestroot --protocol=tcp -P3306 -hmysql mysql -e "SHOW DATABASES;"
-mariadb -hmysql -P3306 -uroot -pbtestroot --protocol=tcp mysql -e "SHOW DATABASES;"
-SQLCMD0="mariadb mysql -uroot -pbtestroot --protocol=tcp -P3306 -hmysql "
-SQLCMD1="mariadb batchertestdb -s -ubtest -pbtest --protocol=tcp -P3306 -hmysql "
-SQLCMD="mariadb batchertestdb -s -ubtest -pbtest --protocol=tcp -P3306 -hmysql -e "
+mariadb -uroot --protocol=tcp -P3306 -hlocalhost mysql -e "SHOW DATABASES;"
+# mariadb -hmysql -P3306 -uroot -pbtestroot --protocol=tcp mysql -e "SHOW DATABASES;"
+SQLCMD0="mariadb mysql -uroot -pbtestroot --protocol=tcp -P3306 -hlocalhost "
+SQLCMD1="mariadb batchertestdb -s -ubtest -pbtest --protocol=tcp -P3306 -hlocalhost "
+SQLCMD="mariadb batchertestdb -s -ubtest -pbtest --protocol=tcp -P3306 -hlocalhost -e "
 
 testcount=0
 passcount=0
@@ -44,7 +28,7 @@ $SQLCMD1 < /tmp/pop_composite.sql > /dev/null 2>&1
 echo ".done"
 
 printf "Starting tests"
-myruntests 3306 "collation=utf8_general_ci" mariadb
+myruntests 3306 "collation=utf8_general_ci" localhost
 echo "done"
 
 echo "MariaDB Tests: $testcount Passed: $passcount Failed: $errorcount"

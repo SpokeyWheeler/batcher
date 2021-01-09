9d1ec9c9dc4ad6c1677e173e95e93d8a1c2585b3
Spokey Wheeler <12411310+SpokeyWheeler@users.noreply.github.com>
2021-01-09 17:02:28 +0000

    ci(mysql): try 0.0.0.0

--- test/mysql.sh
+++ test/mysql.sh
@@ -11,10 +11,10 @@ testcount=0
 passcount=0
 errorcount=0
 
-# SQLCMD0="mysql mysql -uroot -pbtestroot --protocol=tcp -P3306 -hlocalhost "
-# SQLCMD0="mysql mysql -ubtest -pbtest --protocol=tcp -P3306 -hlocalhost "
-SQLCMD0="mysql batchertestdb -s -ubtest -pbtest --protocol=tcp -P3306 -hlocalhost "
-SQLCMD="mysql batchertestdb -s -ubtest -pbtest --protocol=tcp -P3306 -hlocalhost -e "
+# SQLCMD0="mysql mysql -uroot -pbtestroot --protocol=tcp -P3306 -h0.0.0.0 "
+# SQLCMD0="mysql mysql -ubtest -pbtest --protocol=tcp -P3306 -h0.0.0.0 "
+SQLCMD0="mysql batchertestdb -s -ubtest -pbtest --protocol=tcp -P3306 -h0.0.0.0 "
+SQLCMD="mysql batchertestdb -s -ubtest -pbtest --protocol=tcp -P3306 -h0.0.0.0 -e "
 
 printf "Creating test database..."
 $SQLCMD0 < test/mysql1.sql # > /dev/null 2>&1
@@ -26,7 +26,7 @@ $SQLCMD0 < /tmp/pop_composite.sql > /dev/null 2>&1
 echo "done"
 
 printf "Starting tests"
-myruntests 3306 "collation=utf8_general_ci" localhost
+myruntests 3306 "collation=utf8_general_ci" 0.0.0.0
 echo "done"
 
 echo "MySQL Tests: $testcount Passed: $passcount Failed: $errorcount"

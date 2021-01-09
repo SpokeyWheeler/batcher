dc8aaf42d0f0a3bfa16a3abca8c6c39f38993a1a
Spokey Wheeler <12411310+SpokeyWheeler@users.noreply.github.com>
2021-01-09 16:55:25 +0000

    ci(mysql): semaphore creates the user and database for you

--- test/mysql.sh
+++ test/mysql.sh
@@ -12,8 +12,8 @@ passcount=0
 errorcount=0
 
 # SQLCMD0="mysql mysql -uroot -pbtestroot --protocol=tcp -P3306 -hlocalhost "
-SQLCMD0="mysql mysql -uroot -pbtestroot --protocol=tcp -P3306 -hlocalhost "
-SQLCMD1="mysql batchertestdb -s -ubtest -pbtest --protocol=tcp -P3306 -hlocalhost "
+# SQLCMD0="mysql mysql -ubtest -pbtest --protocol=tcp -P3306 -hlocalhost "
+SQLCMD0="mysql batchertestdb -s -ubtest -pbtest --protocol=tcp -P3306 -hlocalhost "
 SQLCMD="mysql batchertestdb -s -ubtest -pbtest --protocol=tcp -P3306 -hlocalhost -e "
 
 printf "Creating test database..."
@@ -21,12 +21,12 @@ $SQLCMD0 < test/mysql1.sql # > /dev/null 2>&1
 echo "done"
 
 printf "Populating test database..."
-$SQLCMD1 < /tmp/pop_serial.sql > /dev/null 2>&1
-$SQLCMD1 < /tmp/pop_composite.sql > /dev/null 2>&1
+$SQLCMD0 < /tmp/pop_serial.sql > /dev/null 2>&1
+$SQLCMD0 < /tmp/pop_composite.sql > /dev/null 2>&1
 echo "done"
 
 printf "Starting tests"
-myruntests 3306 "collation=utf8_general_ci" localhosr
+myruntests 3306 "collation=utf8_general_ci" localhost
 echo "done"
 
 echo "MySQL Tests: $testcount Passed: $passcount Failed: $errorcount"

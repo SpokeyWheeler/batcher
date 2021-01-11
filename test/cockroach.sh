f09a75866559888c97f9b054fcf5921940abcc36
Spokey Wheeler <12411310+SpokeyWheeler@users.noreply.github.com>
2021-01-11 23:20:37 +0000

    fix(cockroach): revert test changes

--- test/cockroach.sh
+++ test/cockroach.sh
@@ -33,8 +33,6 @@ errorcount=0
 printf "Starting tests"
 
 pgruntests 26257 "sslmode=verify-ca&sslrootcert=/tmp/certs/ca.crt&sslcert=/tmp/certs/client.btest.crt&sslkey=/tmp/certs/client.btest.key" localhost "" ""
-pgruntests 26257 "sslmode=verify-ca&sslrootcert=/tmp/certs/ca.crt&sslcert=/tmp/certs/client.btest.crt&sslkey=/tmp/certs/client.btest.key" localhost btest ""
-pgruntests 26257 "sslmode=verify-ca&sslrootcert=/tmp/certs/ca.crt&sslcert=/tmp/certs/client.btest.crt&sslkey=/tmp/certs/client.btest.key" localhost btest btest
 
 echo "done"
 echo "CockroachDB Tests: $testcount Passed: $passcount Failed: $errorcount"

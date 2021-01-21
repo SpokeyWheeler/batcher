92731b23277477606acb278eff9dcdbb5ed1eb45
Spokey Wheeler <12411310+SpokeyWheeler@users.noreply.github.com>
2021-01-21 22:44:14 +0000

    refactor(globbing): remove ambiguity

--- test/informix.sh
+++ test/informix.sh
@@ -77,34 +77,34 @@ ixruntests() {
         exptot=900
 	expa=0
 
-	./batcher update -concurrency 4 -database batchertestdb -dbtype informix -host $host -opts $opts -password in4mix -portnum $portnum -table serialtest -set "strcol='b'" -user informix -where "strcol='a'" -execute
+	./batcher update -concurrency 4 -database batchertestdb -dbtype informix -host "$host" -opts "$opts" -password in4mix -portnum "$portnum" -table serialtest -set "strcol='b'" -user informix -where "strcol='a'" -execute
 
 	sera=$( echo "SELECT COUNT(1) FROM serialtest WHERE strcol = 'a';" | $SQLCMD 2> /dev/null | grep -iv count | grep -iv row | grep -v "^$" | awk '{print $1}' )
 	comp "Updated serial a" "$expa" "$sera"
 
-	./batcher delete -concurrency 4 -database batchertestdb -dbtype informix -host $host -opts $opts -password in4mix -portnum $portnum -table serialtest -user informix -where "intcol<101" -execute
+	./batcher delete -concurrency 4 -database batchertestdb -dbtype informix -host "$host" -opts "$opts" -password in4mix -portnum "$portnum" -table serialtest -user informix -where "intcol<101" -execute
 
 	sertot=$( echo "SELECT COUNT(1) FROM serialtest;" | $SQLCMD 2> /dev/null | grep -iv count | grep -iv row | grep -v "^$" | awk '{print $1}' )
 	comp "Small delete serial total" "$exptot" "$sertot"
 
-	./batcher update -concurrency 4 -database batchertestdb -dbtype informix -host $host -opts $opts -password in4mix -portnum $portnum -set "strcol='b'"  -table compositetest -user informix -where "strcol='a'" -execute
+	./batcher update -concurrency 4 -database batchertestdb -dbtype informix -host "$host" -opts "$opts" -password in4mix -portnum "$portnum" -set "strcol='b'"  -table compositetest -user informix -where "strcol='a'" -execute
 
 	cmpa=$( echo "SELECT COUNT(1) FROM compositetest WHERE strcol = 'a';" | $SQLCMD 2> /dev/null | grep -iv count | grep -iv row | grep -v "^$" | awk '{print $1}' )
 	comp "Updated composite a" "$expa" "$cmpa"
 
-	./batcher delete -concurrency 4 -database batchertestdb -dbtype informix -host $host -opts $opts -password in4mix -portnum $portnum -table compositetest -user informix -where "intcol<101" -execute
+	./batcher delete -concurrency 4 -database batchertestdb -dbtype informix -host "$host" -opts "$opts" -password in4mix -portnum "$portnum" -table compositetest -user informix -where "intcol<101" -execute
 
 	cmptot=$( echo "SELECT COUNT(1) FROM compositetest;" | $SQLCMD 2> /dev/null | grep -iv count | grep -iv row | grep -v "^$" | awk '{print $1}' )
 	comp "Small delete composite total" "$exptot" "$cmptot"
 
 	exptot=0
 
-	./batcher delete -concurrency 4 -database batchertestdb -dbtype informix -host $host -opts $opts -password in4mix -portnum $portnum -table serialtest -user informix -where "1=1" -execute
+	./batcher delete -concurrency 4 -database batchertestdb -dbtype informix -host "$host" -opts "$opts" -password in4mix -portnum "$portnum" -table serialtest -user informix -where "1=1" -execute
 
 	sertot=$( echo "SELECT COUNT(1) FROM serialtest;" | $SQLCMD 2> /dev/null | grep -iv count | grep -iv row | grep -v "^$" | awk '{print $1}' )
 	comp "Full delete serial total" "$exptot" "$sertot"
 
-	./batcher delete -concurrency 4 -database batchertestdb -dbtype informix -host $host -opts $opts -password in4mix -portnum $portnum -table compositetest -user informix -where "1=1" -execute
+	./batcher delete -concurrency 4 -database batchertestdb -dbtype informix -host "$host" -opts "$opts" -password in4mix -portnum "$portnum" -table compositetest -user informix -where "1=1" -execute
 
 	cmptot=$( echo "SELECT COUNT(1) FROM compositetest;" | $SQLCMD 2> /dev/null | grep -iv count | grep -iv row | grep -v "^$" | awk '{print $1}' )
 	comp "Full delete composite total" "$exptot" "$cmptot"

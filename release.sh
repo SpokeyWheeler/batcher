b561699384a417395fdc747fe9f9a45ac91ab03b
Spokey Wheeler <12411310+SpokeyWheeler@users.noreply.github.com>
2021-01-23 20:22:00 +0000

    fix(release.sh): stop goreleaser if this isn't a version bump

--- release.sh
+++ release.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 rm -f split.sh
 git describe
+git log -1 | grep "bump:" > /dev/null 2>&1
+if [ $? -ne 0 ]
+then
+	echo "Not a release"
+	exit 0
+fi
 cd /tmp
 curl -sfL https://install.goreleaser.com/github.com/goreleaser/goreleaser.sh | sh
 cd -
